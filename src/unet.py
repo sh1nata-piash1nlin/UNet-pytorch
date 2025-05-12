67e4486393f9f160911bdaa253c06f8d763b3cdb
Nguyen Duc Tri <113581831+sh1nata-piash1nlin@users.noreply.github.com>
2025-05-12 11:05:51 +0700

    Update unet.py

--- src/unet.py
+++ src/unet.py
@@ -1,69 +1,81 @@
+from torch import nn
 import torch
-import torch.nn as nn
-from torchsummary import summary
-
-class Unet(nn.Module):
-    def __init__(self, num_classes=1):
-        super().__init__()
-        self.num_classes = num_classes
-        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear')
-        self.downsample = nn.MaxPool2d(2, stride=2)
-        self.blockDown1 = self.blockDown(3, 64)
-        self.blockDown2 = self.blockDown(64, 128)
-        self.blockDown3 = self.blockDown(128, 256)
-        self.blockDown4 = self.blockDown(256, 512)
-        self.blockNeck = nn.Sequential(
-            nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-        )
-        self.blockUp1 = self.blockUp(1536, 512)
-        self.blockUp2 = self.blockUp(768, 256)
-        self.blockUp3 = self.blockUp(384, 128)
-        self.blockUp4 = self.blockUp(192, 64)
-        self.final_conv = nn.Conv2d(64, self.num_classes, kernel_size=1)
-
-    def blockDown(self, in_channels, out_channels):
-        return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-        )
-
-    def blockUp(self, in_channels, out_channels):
-        return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ConvBlock, self).__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
         )
 
     def forward(self, x):
-        x1 = self.blockDown1(x)
-        x =  self.downsample(x1)
-        x2 = self.blockDown2(x)
-        x = self.downsample(x2)
-        x3 = self.blockDown3(x)
-        x = self.downsample(x3)
-        x4 = self.blockDown4(x)
-        x = self.downsample(x4)
-        x = self.blockNeck(x)
-        x = torch.cat([x4, self.upsample(x)], dim=1)
-        x = self.blockUp1(x)
-        x = torch.cat([x3, self.upsample(x)], dim=1)
-        x = self.blockUp2(x)
-        x = torch.cat([x2, self.upsample(x)], dim=1)
-        x = self.blockUp3(x)
-        x = torch.cat([x1, self.upsample(x)], dim=1)
-        x = self.blockUp4(x)
-        x = self.final_conv(x)
-        return x
+        return self.block(x)
 
-if __name__ == '__main__':
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = Unet()
-    model.to(device)
-    model.train()
-    summary(model, (3, 384, 384))
+
+class DownConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DownConv, self).__init__()
+        self.sequence = nn.Sequential(
+            ConvBlock(in_channels, out_channels),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+    def forward(self, x):
+        return self.sequence(x)
+
+
+class UpConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UpConv, self).__init__()
+        self.sequence = nn.Sequential(
+            nn.ConvTranspose2d(in_channels, in_channels, kernel_size=2, stride=2),
+            ConvBlock(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.sequence(x)
+
+
+class UNet(nn.Module):
+    def __init__(self, in_channels=3, out_channels=1):
+        super(UNet, self).__init__()
+        # input_dim = 256
+        self.encoder = nn.ModuleList([
+            DownConv(in_channels, 64),  # 128
+            DownConv(64, 128),  # 64
+            DownConv(128, 256),  # 32
+            DownConv(256, 512)  # 16
+        ])
+
+        self.bottleneck = ConvBlock(512, 1024)
+
+        # extra channels allow for concatenation of skip connections in upsampling block
+        self.decoder = nn.ModuleList([
+            UpConv(512 + 1024, 512),  # 32
+            UpConv(256 + 512, 256),  # 64
+            UpConv(128 + 256, 128),  # 128
+            UpConv(64 + 128, 64)  # 256
+        ])
+
+        self.output_conv = nn.Conv2d(64, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        skips = []
+        o = x
+        for layer in self.encoder:
+            o = layer(o)
+            skips.append(o)
+
+        o = self.bottleneck(o)
+
+        for i, layer in enumerate(self.decoder):
+            # print(o.size())
+            o = torch.cat((skips[len(skips) - i - 1], o), dim=1)
+            # print(o.size())
+            o = layer(o)
+
+        return self.output_conv(o)
